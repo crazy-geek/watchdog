07bd09a98a07f15997a3cc475e805c1a6208bac2
Jijo Joy <jijojoy@gmail.com>
2018-04-19 08:24:06 +0530

    passport intro

--- user.js
+++ user.js
@@ -3,51 +3,65 @@ var passport = require('passport');
 var localStrategy = require('passport-local').Strategy;
 var User = require('./models/user');
 
-module.exports = function(data, callback){
-    mongoose.connect('mongodb://localhost:27017/authentication',(err)=>{
-        if(err)
+module.exports = function (data, callback) {
+    mongoose.connect('mongodb://localhost:27017/authentication', (err) => {
+        if (err)
             console.log(err);
     })
-     this.add = (data,callback) =>{
-        var user = User.findOne({'email':'jijojoy1@gmail.com'},(err, result) =>{
-            console.log(result);
-            if(err){
+    this.add = (data, callback) => {
+        var user = User.findOne({ 'email': 'jijojoy1@gmail.com' }, (err, result) => {
+            //console.log(result);
+            if (err) {
                 return callback(err, null);
             }
-            if(result !== null)
+            if (result !== null)
                 return callback('user already exists', null);
-            else{
+            else {
                 var newuser = new User({
-                    full_name:'Jijo Joy',
+                    full_name: 'Jijo Joy',
                     phone: '1234567',
-                    email:'jijojoy1@gmail.com',
-                    password:'pwd'
-                }).save((err,res) => {
-                    callback(err,res);
+                    email: 'jijojoy1@gmail.com',
+                    password: 'pwd'
+                }).save((err, res) => {
+                    callback(err, res);
                 })
             }
-        });        
+        });
 
-        
+
     };
 
 
-     this.remove = (id, callback)=>{
-
-     };
-
-     this.findById = (id) =>{
+    this.remove = (id, callback) => {
 
     };
 
-     this.findAll = (limit, page) => {
+    this.findById = (id) => {
 
     };
 
-    this.login = (data, callback)=>{
-        passport.authenticate(localStrategy, 
-                            {successRedirect:'/', failiourRedirect:'/user/login1'});
+    this.findAll = (limit, page) => {
+
+    };
+
+    this.login = (data, callback) => {
+
+
         callback(null, 'success');
     }
 };
+
+passport.use(new localStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    session: false
+}),
+    (username, password, done) => {
+        User.findOne({ email: 'jijojoy@gmail.com' }, (err, result) => {
+            if (result == null) { }
+        })
+});
+
+
+
 //module.exports = {save,remove,findById,findAll}
