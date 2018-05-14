50da61f51d41d16730cd720e60a46afd5e7942a8
Jijo <jjoy@suyati.com>
2018-05-14 17:35:29 +0530

    removed console.logs

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,9 +1,10 @@
-const passport = require('passport');
+
 
 module.exports = {
     verifyToken : async (req,res,next) =>{
         let user = req.user;
-        console.log(user);
+        if(!req.user)
+            return req.status(401).json({error:'unauthorized'})
         return res.status(200).json({'validToken':true});    
     }
 }
